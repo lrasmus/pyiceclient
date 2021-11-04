9d2d4f274104dfe9fd86f334850850912c149147
Luke Rasmussen <luke.rasmussen@gmail.com>
2021-11-04 13:26:42 -0500

    Project code and documentation

--- README.md
+++ README.md
@@ -1,4 +1,66 @@
-Python ICE Client (pyiceclient)
+# OpenMRS / ICE - AMIA 2021 Connectathon Project
+
+This was a brief exploration/proof-of-concept project conducted as part of the AMIA 2021 Annual Symposium workshop: "W07: Connectathon: Making Open-Source Global Health Systems Talk to Each Other".  This event was organized by the [AMIA Global Health Informatics Working Group](https://amia.org/community/working-groups/global-health-informatics), and sponsored in part by the [AMIA Open Source Working Group](https://amia.org/community/working-groups/open-source).
+
+## Scope
+
+The scope of this project was very minimal:
+
+1. Try out immunization data available for testing in [OpenMRS](https://openmrs.org/)
+2. See if we can retrieve immunization data from OpenMRS and connect it to the [Immunization Calculation Engine (ICE)](https://www.hln.com/services/open-source/ice/index.html)
+
+## Setup
+
+I grabbed a copy of the [ICE Docker container](https://hub.docker.com/r/hlnconsulting/ice) using the instructions provided.
+
+```
+docker pull hlnconsulting/ice:latest
+docker run --log-opt max-size=100m --log-opt max-file=5 --rm -d -p 32775:8080 --name ice hlnconsulting/ice:latest
+```
+
+If you want to keep an eye on the ICE logs while you're testing, run:
+
+```
+docker logs -f ice
+```
+
+You will need to modify [tests/test_pyiceclient.py](tests/test_pyiceclient.py) with the OpenMRS credentials [found on the OpenMRS Demo website](https://openmrs.org/demo/).  Adjust the endpoints as well, as needed.
+
+From here, just run:
+
+```
+python tests/test_pyiceclient.py
+```
+
+
+## AMIA 2021 Connectathon Notebook
+
+This got ICE running locally for me with an active API at `http://localhost:32775/opencds-decision-support-service/api/resources/evaluate`.  I tested that it was working correctly by [running `curl-rest-tests`](https://github.com/cdsframework/ice/tree/master/curl-rest-tests).
+
+Next I wanted to do something more with ICE.  This repository is a modified fork of [pyiceclient](https://github.com/cdsframework/pyiceclient).  Some of the envelope for the requests had changed, so [I made a few changes](https://github.com/lrasmus/pyiceclient/commit/29952c866f137aafaa1823ec16bd00314468d25e) until it worked.
+
+From here I needed to take the data from OpenMRS (available via FHIR) and convert it to the [vMR format that ICE wants](https://github.com/cdsframework/pyiceclient#ice-web-client-data-structure).  This was relatively straightforward to do, thanks to great documentation!
+
+Now it was time to get the Immunization data.  Here I ran into a few snags.  It was easy enough to get the Immunization records from OpenMRS, but the CVX code and vaccine details didn't seem to be populated.  It was around this point that I ran out of time so I just hardcoded `"127: H1N1-09, injectable"`.  This at least lets us prove data can be sent and returned.
+
+Then I just needed to run the test app:
+
+```
+python tests/test_pyiceclient.py
+```
+
+It wrote results to `test_out.json`, and you can review in there the vaccine schedule.
+
+### Thoughts and Next Steps
+
+For me this was a great learning experience.  I got some hands-on time with OpenMRS and ICE.  Both systems are really well documented and were very easy to get set up and running.  That's to say, all of this work should be credited to those projects and developers - I did very little (outside of learning) thanks to them.
+
+This shows that we can easily bridge from OpenMRS to ICE and get JSON results.  But we need to get this actually in OpenMRS.  I don't know what has been done already, so I didn't want to go too far ahead with what integration may be best for the community.
+
+[ICE has a really nice UI](https://cds.hln.com/iceweb/), so it may be better to try and connect with that UI directly instead of recreating it using the JSON response.  But, both options seem possible.
+
+
+Original README - Python ICE Client (pyiceclient)
 ===============================
 
 A Python 3 module to convert ICE web client data structure to vMR,
--- tests/test_pyiceclient.py
+++ tests/test_pyiceclient.py
@@ -1,29 +1,106 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Python3 ICE client demo/test app:
+"""Python3 OpenMRS/ICE test app:
 
-Take a test.json file saved by the ICE web client, send it through
+Get patient information from OpenMRS, format and  send it through
 ICE, add the evaluations and forecasts and re-save it as test_out.json.
 
 """
 
+# Original information retained - modifications made by https://github.com/lrasmus
+# in 2021.
 __author__      = "HLN Consulting, LLC"
 __copyright__   = "Copyright 2018, HLN Consulting, LLC"
 __license__     = "BSD-2-Clause"
 
+import requests
 import pyiceclient
 import json
 import datetime
 
-ICE_SERVICE_ENDPOINT = 'https://cds.hln.com/opencds-decision-support-service/evaluate'
+SESS = requests.Session()
+# Get the actual values from https://openmrs.org/demo/
+# (I know it's publicly available, but still felt weird committing credentials)
+SESS.auth = ('user', 'pass')
 
+# This assumes you're running ICE in a Docker container locally
+ICE_SERVICE_ENDPOINT = 'http://localhost:32775/opencds-decision-support-service/api/resources/evaluate'
+OPEN_MRS_ENDPOINT = 'https://openmrs-spa.org/openmrs/ws/fhir2/R4/'
 
-with open('tests/test.json') as json_data:
-    data = json.load(json_data)
+# Given a patient identifier within OpenMRS (assumed to be the FHIR id, not the MRN)
+# create a vMR structure that can be used by ICE.
+# This is just a simple FHIR -> vMR translator
+def get_vmr_patient(patient_id):
+  openmrs_patient_uri = OPEN_MRS_ENDPOINT + 'Patient/' + patient_id
+  headers={
+    'Content-type':'application/json',
+    'Accept':'application/json'
+  }
+  req = SESS.get(openmrs_patient_uri, headers=headers)
+  rspstr = req.content.decode('utf-8')
+  if req.status_code == 200:
+    patient = json.loads(rspstr)
+    return {
+      "id": patient['identifier'][0]['value'],
+      "firstName": patient['name'][0]['given'][0],
+      "lastName": patient['name'][0]['family'],
+      "gender": patient['gender'],
+      "dob": patient['birthDate'].replace('-', ''),
+      "evalDate": datetime.date.today().strftime('%Y%m%d'),
+      "izs": [],
+      "evaluations": [],
+      "forecasts": []
+    }
+  else:
+    print('Error: ' + str(req))
+    return "Error"
+
+# Given a patient identifier within OpenMRS (assumed to be the FHIR id, not the MRN)
+# retrieve the immunization records for the patient and format them in the vMR format
+# required by ICE.
+# This returns the array of immunizations, not the entire vMR structure
+def get_vmr_immunizations(patient_id):
+  openmrs_patient_uri = OPEN_MRS_ENDPOINT + 'Immunization?patient=' + patient_id
+  headers={
+    'Content-type':'application/json',
+    'Accept':'application/json'
+  }
+  req = SESS.get(openmrs_patient_uri, headers=headers)
+  rspstr = req.content.decode('utf-8')
+  if req.status_code != 200:
+    print('Error: ' + str(req))
+    return "Error"
+
+  response = json.loads(rspstr)
+  if response is None or response['total'] == 0:
+    return []
+
+  ice_immunizations = []
+  for immunization in response['entry']:
+    ice_immunizations.append([
+      immunization['resource']['id'],
+      datetime.datetime.strptime(immunization['resource']['occurrenceDateTime'], '%Y-%m-%dT%H:%M:%S%z').strftime('%Y%m%d'),
+      # TODO: I didn't see these details in Immunization resources yet in OpenMRS
+      "127: H1N1-09, injectable",
+      "I"
+    ])
+  return ice_immunizations
+
+# Hardcoding a test patient ID for now, since it's a POC
+patient_id = '5b331b80-2681-4429-baee-9c39ba51c164'
+patient = get_vmr_patient(patient_id)
+immunizations = get_vmr_immunizations(patient_id)
+patient['izs'] = immunizations
+
+data = [patient]
+
+# Leaving this here in case we ever want to go back to testing from provided sample data
+#with open('tests/test.json') as json_data:
+#    data = json.load(json_data)
 
 request_vmr = pyiceclient.data2vmr(data)
-response_vmr = pyiceclient.send_request(request_vmr, ICE_SERVICE_ENDPOINT, datetime.date.today().strftime('%Y-%m-%d'))
+response_vmr = pyiceclient.send_request(request_vmr, ICE_SERVICE_ENDPOINT)
 (evaluation_list, forecast_list) = pyiceclient.process_vmr(response_vmr)
 data[0]['evaluations'] = evaluation_list
 data[0]['forecasts'] = forecast_list
